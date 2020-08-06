661e2ff5501110a09b2c2142b0129f6c25a09fff
Igor Caviroski <icavir@gmail.com>
2020-08-07 00:39:51 +0200

    add yaml

--- read.dart
+++ read.dart
@@ -1,7 +1,29 @@
+import 'package:args/args.dart';
+
 void main(List<String> arguments) {
   exitCode = 0; // presume success
   final parser = ArgParser();
 
   argResults = parser.parse(arguments);
   final paths = argResults.rest;
+}
+
+Future readFile(List<String> paths) async {
+  if (paths.isEmpty) {
+    // No files provided as arguments. Read from stdin and print each line.
+    await stdin.pipe(stdout);
+  } else {
+    for (var path in paths) {
+      final lines = utf8.decoder
+          .bind(File(path).openRead())
+          .transform(const LineSplitter());
+      try {
+        await for (var line in lines) {
+          stdout.writeln(line);
+        }
+      } catch (_) {
+        await _handleError(path);
+      }
+    }
+  }
 }
