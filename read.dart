409b597ca9f546fef2481eda44889e63801ad0a1
Igor Caviroski <icavir@gmail.com>
2020-08-07 21:33:39 +0200

    handle titles with coma inside

--- read.dart
+++ read.dart
@@ -73,7 +73,20 @@ getTitlePosition(String line) {
 getTitle(String line) {
   var lineElements = line.split(',');
   var title = lineElements.elementAt(titlePosition);
-  creteLink(title);
+  if (title[0] == '"') {
+    getTitleConatainingComa(line, title);
+  } else {
+    creteLink(title);
+  }
+}
+
+getTitleConatainingComa(String line, String title) {
+  var startIndex = line.indexOf(title);
+  var inBetweenTitle = line.substring(startIndex);
+  inBetweenTitle = inBetweenTitle.replaceFirst('"', '');
+  var endIndex = inBetweenTitle.indexOf('"');
+  var newTitle = inBetweenTitle.substring(0, endIndex);
+  creteLink(newTitle);
 }
 
 creteLink(String title) {
