5b2ff6a2cc00a4dcf1e0625f0528da818beb8442
Igor Caviroski <icavir@gmail.com>
2020-08-08 21:43:47 +0200

    use Classes structure

---
+++ bin/imdb_to_netflix.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+
+import 'package:args/args.dart';
+import 'package:imdb_to_netflix/read.dart';
+
+ArgResults argResults;
+
+void main(List<String> arguments) {
+  exitCode = 0; // presume success
+  final parser = ArgParser();
+
+  argResults = parser.parse(arguments);
+  final paths = argResults.rest;
+
+  var read = new Read(); 
+  read.readFile(paths);
+}
